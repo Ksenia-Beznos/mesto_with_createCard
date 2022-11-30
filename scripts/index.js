922642f636699353d40c2c83db8d4437d8d662db
Kseniia Beznos <ksenia.beznos@yandex.ru>
2022-11-30 19:46:27 +0300

    Добавить иконку карзины и написать скрипт для новых 6 карточек

--- scripts/index.js
+++ scripts/index.js
@@ -30,4 +30,76 @@ function formSubmitHandler (evt) {
 
 formElement.addEventListener('submit', formSubmitHandler);
 
+//==================================
+
+const  initialCards = [
+  {
+    text: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    text: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    text: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    text: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    text: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    text: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+];
+
+const makeElement = function(tagName, className, text){
+  const element = document.createElement(tagName);
+  element.classList.add(className);
+  element.textContent = text;
+
+  return element;
+};
+
+const createCard = function(card){
+  const listItem = makeElement('li', 'element__item');//создали новый элемент списка для вставки изображения
+
+
+  const img = makeElement('img', 'element__image'); // создаем переменную для новой карточки
+  img.src = card.link; // добавляем ссыку на карточку
+  img.alt = 'Вид реки зимой, которую окружает заснеженный лес'; //добавляем alt
+  listItem.appendChild(img); //добавляем внутрь li -> img
+
+  const divGroup = makeElement('div', 'element__mask-group'); // создаем переменную для тега div
+  listItem.appendChild(divGroup); //добавляем внутрь li -> div
+
+  const imageTitle = makeElement('h2', 'element__title', card.text); // создаем переменную для h2
+  divGroup.appendChild(imageTitle); //добавляем внутрь divGroup -> h2 (переменная imageTitle)
+
+  const heartButton = document.createElement('button'); // создаем переменную для  button
+  heartButton.classList.add('element__heart-button'); //присвоим класс для button (переменная imageButton)
+  heartButton.type = 'button';
+  divGroup.appendChild(heartButton); //добавляем внутрь divGroup -> button (переменная imageTitle)
+
+  const trashButton = document.createElement('button'); // создаем переменную для button
+  trashButton.classList.add('element__trash-button'); //присвоим класс для button
+  trashButton.type = 'button';
+  listItem.appendChild(trashButton); //добавляем иконку trashButton в li
+
+  return listItem;
+};
+
+const cardList = document.querySelector('.element'); //находим список ul с классом element, чтобы туда поместить блок с новой картинкой
+
+for (let i = 0; i < initialCards.length; i ++){
+  let cardItem = createCard(initialCards[i]);
+  cardList.appendChild(cardItem);
+}
+
 
