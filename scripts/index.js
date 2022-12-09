a76e69b3f456cd5c66727c0c367813e8856b12db
Kseniia Beznos <ksenia.beznos@yandex.ru>
2022-12-09 14:14:47 +0300

    Сделать лайки и удаление по клику на иконки

--- scripts/index.js
+++ scripts/index.js
@@ -32,7 +32,7 @@ formElement.addEventListener('submit', formSubmitHandler);
 //==================================
 
 const popupCards = document.querySelector('.popup-cards'); //попап для рекдактирования фото
-const openButtonCards = document.querySelector('.profile__add-button'); //кнопка открытия
+const addButtonCards = document.querySelector('.profile__add-button'); //кнопка открытия
 const closeButtonCards = document.querySelector('.popup-cards__close-icon'); //кнопка закрытия
 
 const titleInput = document.querySelector('.popup-cards__input_type_title');
@@ -40,7 +40,7 @@ const linkInput = document.querySelector('.popup-cards__input_type_link');
 
 const formElementCards = document.querySelector('.popup-cards__form');
 
-openButtonCards.addEventListener('click', function() {
+addButtonCards.addEventListener('click', function() {
   popupCards.classList.add('popup-cards_opened');
 });
 
@@ -98,10 +98,9 @@ const makeElement = function(tagName, className, text){
 const createCard = function(card){
   const listItem = makeElement('li', 'element__item');//создали новый элемент списка для вставки изображения
 
-
   const img = makeElement('img', 'element__image'); // создаем переменную для новой карточки
   img.src = card.link; // добавляем ссыку на карточку
-  img.alt = 'Вид реки зимой, которую окружает заснеженный лес'; //добавляем alt
+  img.alt = card.text; //добавляем alt
   listItem.appendChild(img); //добавляем внутрь li -> img
 
   const divGroup = makeElement('div', 'element__mask-group'); // создаем переменную для тега div
@@ -130,4 +129,21 @@ for (let i = 0; i < initialCards.length; i ++){
   cardList.appendChild(cardItem);
 }
 
+// ==============
+
+const element = document.querySelector('.element'); //находим родительский элемент
+
+//Вешаем на родительский элемент обработчик событий лайка (поставить/удалить)
+element.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('element__heart-button')) { // создаем условие - если есть evt.target и класс 'element__heart-button', то при нажатии происходит переключение на класс _active, и наоборот
+    evt.target.classList.toggle('element__heart-button_active'); // toogle - переключает один класс на другой при клике
+  }
+});
+
+//Вешаем на родительский элемент обработчик событий удаления карточки
+element.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('element__trash-button')) { // создаем условие - если есть evt.target и класс 'element__trash-button', то при нажатии на кнопку происходит удаление родительского элемента
+    evt.target.closest('.element__item').remove();
+  }
+});
 
