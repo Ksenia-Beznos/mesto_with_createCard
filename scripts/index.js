8cd0a18653cca59e0932d075f3562ae2bf256e09
Kseniia Beznos <ksenia.beznos@yandex.ru>
2022-12-12 15:54:04 +0300

    Mesto(part-2).Hotfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
-const popup = document.querySelector('.popup'); //попап для рекдактирования текстовой информации
-const openButton = document.querySelector('.profile__edit-button'); //кнопка открытия
-const closeButton = document.querySelector('.popup__close-icon'); //кнопка закрытия
+// popup для редактирования данных в профиле
+
+const popupProfile = document.querySelector('.popup'); //попап для рекдактирования текстовой информации
+const popupProfileOpenButton = document.querySelector('.profile__edit-button'); //кнопка открытия
+const popupProfileCloseButton = document.querySelector('.popup__close-icon'); //кнопка закрытия
 
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
@@ -8,167 +10,124 @@ const jobInput = document.querySelector('.popup__input_type_job');
 const titleProfile = document.querySelector('.profile__title');
 const subtitleProfile = document.querySelector('.profile__subtitle');
 
-const formElement = document.querySelector('.popup__form');
+const popupFormElement = document.querySelector('.popup__form');
 
-openButton.addEventListener('click', function() {
-  popup.classList.add('popup_opened');
+// общие функции для открытия/закрытия всех попапов
+function openPopup(popup) {
+  popup.classList.add('popup_opened')
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened')
+}
+
+popupProfileOpenButton.addEventListener('click', function() {
+  openPopup(popupProfile);
   nameInput.value = titleProfile.textContent;
   jobInput.value = subtitleProfile.textContent;
 });
 
-closeButton.addEventListener('click', function() {
-  popup.classList.remove('popup_opened');
+popupProfileCloseButton.addEventListener('click', function() {
+  closePopup(popupProfile)
 });
 
-function formSubmitHandler (evt) {
+function profileFormSubmitHandler (evt) {
   evt.preventDefault();
   titleProfile.textContent = nameInput.value;
   subtitleProfile.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  closePopup(popupProfile);
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-
+popupFormElement.addEventListener('submit', profileFormSubmitHandler);
 
 //==================================
 
-const popupCards = document.querySelector('.popup-cards'); //попап для рекдактирования фото
-const addButtonCards = document.querySelector('.profile__add-button'); //кнопка открытия
-const closeButtonCards = document.querySelector('.popup-cards__close-icon'); //кнопка закрытия
+// popup для добавления карточек на страницу
 
-const titleInput = document.querySelector('.popup-cards__input_type_title');
-const linkInput = document.querySelector('.popup-cards__input_type_link');
+const popupCards = document.querySelector('.popup_type_cards'); //попап для добавления фото
+const popupCardsAddButton = document.querySelector('.profile__add-button'); //кнопка открытия
+const popupCardsCloseButton = document.querySelector('.popup__close-icon_type_cards'); //кнопка закрытия
 
-const formElementCards = document.querySelector('.popup-cards__form');
+const titleInput = document.querySelector('.popup__input_type_title');
+const linkInput = document.querySelector('.popup__input_type_link');
 
-const cardList = document.querySelector('.element'); //находим список ul с классом element, чтобы туда поместить блок с новой карточкой
+const formElementCards = document.querySelector('.popup__form_type_cards');
 
-addButtonCards.addEventListener('click', function() {
-  popupCards.classList.add('popup-cards_opened');
+const cardsContainer = document.querySelector('.element'); //находим список ul с классом element, чтобы туда поместить блок с новой карточкой
+
+popupCardsAddButton.addEventListener('click', function() {
+  openPopup(popupCards);
 });
 
-closeButtonCards.addEventListener('click', function() {
-  popupCards.classList.remove('popup-cards_opened');
+popupCardsCloseButton.addEventListener('click', function() {
+  closePopup(popupCards);
 });
 
-function formSubmitHandlerCard (evt) {
+function cardFormSubmitHandler (evt) {
   evt.preventDefault();
   const newCard = {text: titleInput.value, link: linkInput.value};
   const cardItem = createCard(newCard);
-  cardList.prepend(cardItem);
-  popupCards.classList.remove('popup-cards_opened');
+  cardsContainer.prepend(cardItem);
+  closePopup(popupCards);
 }
 
-formElementCards.addEventListener('submit', formSubmitHandlerCard);
-
+formElementCards.addEventListener('submit', cardFormSubmitHandler);
 
 //==================================
 
-
-const  initialCards = [
-  {
-    text: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    text: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    text: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    text: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    text: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    text: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-];
-
-// функция создания элемента
-const makeElement = function(tagName, className, text){
-  const element = document.createElement(tagName);
-  element.classList.add(className);
-  element.textContent = text;
-
-  return element;
-};
-
-// функция создания карточки. Динамически собирается на странице браузера
+// функция создания карточки (через метод template)
 const createCard = function(card){
-  const listItem = makeElement('li', 'element__item');//создаем новый li
+  const listItemTemplate = document.querySelector('#template-element').content;
+  const cardElement = listItemTemplate.cloneNode(true);
 
-  const img = makeElement('img', 'element__image'); // создаем переменную для новой карточки
-  img.src = card.link; // добавляем ссылку на карточку
-  img.alt = card.text; //добавляем alt
-  listItem.appendChild(img); //добавляем внутрь li -> img
+  cardElement.querySelector('.element__image').src = card.link; // добавляем ссылку на карточку
+  cardElement.querySelector('.element__image').alt = card.text; // добавляем текст в alt
+  cardElement.querySelector('.element__title').textContent = card.text; // добавляем название карточки
 
-  const divGroup = makeElement('div', 'element__mask-group'); // создаем переменную для тега div
-  listItem.appendChild(divGroup); //добавляем внутрь li -> div
-
-  const imageTitle = makeElement('h2', 'element__title', card.text); // создаем переменную для h2
-  divGroup.appendChild(imageTitle); //добавляем внутрь divGroup -> h2 (переменная imageTitle)
-
-  const heartButton = document.createElement('button'); // создаем переменную для  button
-  heartButton.classList.add('element__heart-button'); //присвоим класс для button (переменная imageButton)
-  heartButton.type = 'button';
-  divGroup.appendChild(heartButton); //добавляем внутрь divGroup -> button (переменная imageTitle)
-
-  const trashButton = document.createElement('button'); // создаем переменную для button
-  trashButton.classList.add('element__trash-button'); //присвоим класс для button
-  trashButton.type = 'button';
-  listItem.appendChild(trashButton); //добавляем иконку trashButton в li
-
-  return listItem;
+  return cardElement;
 };
 
-for (let i = 0; i < initialCards.length; i ++){
-  let cardItem = createCard(initialCards[i]);
-  cardList.appendChild(cardItem); // добавление карточки на страницу
-}
+initialCards.forEach((element) => {
+  const card = createCard(element);
+  cardsContainer.prepend(card)
+})
 
 // ==============
-const element = document.querySelector('.element'); //находим родительский элемент кнопок на карточке
 
-const popupImage = document.querySelector('.popup-image');
-const openButtonImage = popupImage.querySelector('.element__image'); // попап с картинкой открывается при нажатии на изображение
-const closeButtonImage = popupImage.querySelector('.popup-image__close-icon'); // кнопка закрытия
-const popupImagePhoto = popupImage.querySelector('.popup-image__photo');
-const popupImageFigcap = popupImage.querySelector('.popup-image__figcap');
+// popup для открытия картинок в полномасштабном размере
+
+const popupImage = document.querySelector('.popup_type_image'); // попап для открытия картинок
+const popupImageOpenButton = popupImage.querySelector('.element__image'); // попап с картинкой открывается при нажатии на изображение
+const popupImageCloseButton = popupImage.querySelector('.popup__close-icon_type_image'); // кнопка закрытия
+const popupImagePhoto = popupImage.querySelector('.popup__photo');
+const popupImageFigcap = popupImage.querySelector('.popup__figcap');
 
 // открытие попапа с картинкой при клике на изображение
-element.addEventListener('click', (evt) => {
+cardsContainer.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('element__image')) {
     popupImagePhoto.src=evt.target.src;
     popupImagePhoto.alt=evt.target.alt;
     popupImageFigcap.textContent=evt.target.alt;
-    popupImage.classList.add('popup-image_opened');
+    openPopup(popupImage);
   }
 });
 
-// закрытие попапа с картинкой
-closeButtonImage.addEventListener('click', function() {
-  popupImage.classList.remove('popup-image_opened');
+// закрытие попапа с картинкой при клике на кнопку закрытия
+popupImageCloseButton.addEventListener('click', function() {
+  closePopup(popupImage);
 });
 
 // ==============
 
 //Вешаем на родительский элемент (element) обработчик событий лайка (поставить/удалить)
-element.addEventListener('click', (evt) => {
+cardsContainer.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('element__heart-button')) { // создаем условие - если есть evt.target и класс 'element__heart-button', то при нажатии происходит переключение на класс _active, и наоборот
     evt.target.classList.toggle('element__heart-button_active'); // toogle - переключает один класс на другой при клике
   }
 });
 
 //Вешаем на родительский элемент обработчик событий удаления карточки
-element.addEventListener('click', (evt) => {
+cardsContainer.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('element__trash-button')) { // создаем условие - если есть evt.target и класс 'element__trash-button', то при нажатии на кнопку происходит удаление элемента
     evt.target.closest('.element__item').remove();
   }
